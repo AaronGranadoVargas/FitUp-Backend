7d7eef10afa6186e165f00fb2c8aacd11d34a8a3
Andreas <andreasstrainu@gmail.com>
2026-03-31 18:42:55 +0200

    archivos de Producto hechos

--- FitUp-BackEnd/src/main/java/tfg/repository/ProductoRepository.java
+++ FitUp-BackEnd/src/main/java/tfg/repository/ProductoRepository.java
@@ -1,4 +1,11 @@
 package tfg.repository;
 
-public class ProductoRepository {
+import tfg.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    // Aquí más adelante puedes añadir métodos customizados,
+    // como: List<Producto> findByCategoria(Categoria categoria);
 }
--- FitUp-BackEnd/src/main/java/tfg/service/ProductoService.java
+++ FitUp-BackEnd/src/main/java/tfg/service/ProductoService.java
@@ -1,4 +1,23 @@
 package tfg.service;
 
+import tfg.model.Producto;
+import tfg.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductoService {
+
+    private final ProductoRepository productoRepository;
+
+    @Autowired
+    public ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
+    public List<Producto> obtenerTodosLosProductos() {
+        return productoRepository.findAll();
+    }
 }
