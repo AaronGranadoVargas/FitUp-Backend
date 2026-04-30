ac0264e88a7c92ceae3f13e0ac59d636907381e0
Aaron <aarongranadovargas@gmail.com>
2026-04-30 15:01:08 +0200

    Arreglo proyecto, cambio de clase SecurityConfig

--- FitUp-BackEnd/src/main/java/tfg/security/SecurityConfig.java
+++ FitUp-BackEnd/src/main/java/tfg/security/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 // Estas dos líneas son nuevas para el CORS
 import org.springframework.web.cors.CorsConfiguration;
+
 import java.util.List;
 
 @Configuration
@@ -26,7 +28,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // 1. AÑADIMOS EL CORS: Le decimos al portero que deje pasar a React Native (localhost)
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     configuration.setAllowedOrigins(List.of("*"));
@@ -34,9 +35,8 @@ public class SecurityConfig {
                     configuration.setAllowedHeaders(List.of("*"));
                     return configuration;
                 }))
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        // 2. AÑADIMOS "/api/usuarios/**": Para que la ruta de tu Login no pida token
                         .requestMatchers("/api/auth/**", "/api/usuarios/**").permitAll()
                         .anyRequest().authenticated()
                 )
